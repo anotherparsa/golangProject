a5e45bcebd7b42ed6a8efbc8d2e5b39f4522cfdf
anotherparsa <anotherparsa@gmail.com>
2024-08-06 11:57:09 +0330

    serving static files

--- simpleTODO/cmd/web/main.go
+++ simpleTODO/cmd/web/main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	http.HandleFunc("/", router.RoutingHandler)
 
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../../pkg/static/"))))
+
 	fmt.Println("Running server on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
