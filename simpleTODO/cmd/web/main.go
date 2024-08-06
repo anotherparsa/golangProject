b4575ed0bc62f4260fd35cb127a9ada2013537a8
anotherparsa <anotherparsa@gmail.com>
2024-08-06 11:35:35 +0330

    running the server

--- simpleTODO/cmd/web/main.go
+++ simpleTODO/cmd/web/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world")
+	})
+
+	fmt.Println("Running server on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
