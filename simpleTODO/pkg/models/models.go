1b0c45c1cb536bd46b26e8fdb39d720aecf1f33e
anotherparsa <anotherparsa@gmail.com>
2024-08-14 18:07:30 +0330

    making models for users, tasks, sessions

---
+++ simpleTODO/pkg/models/models.go
@@ -0,0 +1,27 @@
+package models
+
+type User struct {
+	ID          int
+	UserId      string
+	Username    string
+	Password    string
+	FirstName   string
+	LastName    string
+	Email       string
+	PhoneNumber string
+}
+
+type Task struct {
+	Id          int
+	Author      string
+	Priority    string
+	Title       string
+	Description string
+	IsDone      string
+}
+
+type Session struct {
+	Id        int
+	SessionId string
+	UserId    string
+}
