38ec02937f6d3822120e464db966b5f55393bbb5
anotherparsa <anotherparsa@gmail.com>
2024-08-18 23:51:13 +0330

    1-TwoSum leetcode

--- leetcode/1-TwoSum/main.go
+++ leetcode/1-TwoSum/main.go
@@ -2,7 +2,25 @@ package main
 
 import "fmt"
 
-func twoSum(numbers []int, target int) []int {
+func main() {
+	numbers1 := []int{2, 7, 11, 15}
+	target1 := 9
+	fmt.Println(twoSumFirst(numbers1, target1))
+	fmt.Println(twoSumFSecond(numbers1, target1))
+
+	numbers2 := []int{3, 2, 4}
+	target2 := 6
+
+	fmt.Println(twoSumFirst(numbers2, target2))
+	fmt.Println(twoSumFSecond(numbers2, target2))
+
+	numbers3 := []int{3, 3}
+	target3 := 6
+	fmt.Println(twoSumFirst(numbers3, target3))
+	fmt.Println(twoSumFSecond(numbers3, target3))
+}
+
+func twoSumFirst(numbers []int, target int) []int {
 	for i := 0; i < len(numbers); i++ {
 		for j := i + 1; j < len(numbers); j++ {
 			if numbers[i]+numbers[j] == target {
@@ -13,17 +31,14 @@ func twoSum(numbers []int, target int) []int {
 	return nil
 }
 
-func main() {
-	numbers1 := []int{2, 7, 11, 15}
-	target1 := 9
-	fmt.Println(twoSum(numbers1, target1))
-
-	numbers2 := []int{3, 2, 4}
-	target2 := 6
-
-	fmt.Println(twoSum(numbers2, target2))
-
-	numbers3 := []int{3, 3}
-	target3 := 6
-	fmt.Println(twoSum(numbers3, target3))
+func twoSumFSecond(numbers []int, target int) []int {
+	alreadySeenNumbers := map[int]int{}
+	for i := 0; i < len(numbers); i++ {
+		x := target - numbers[i]
+		if value, ok := alreadySeenNumbers[x]; ok {
+			return []int{value, i}
+		}
+		alreadySeenNumbers[numbers[i]] = i
+	}
+	return nil
 }
