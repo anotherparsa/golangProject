237a5361bb55d28fe0fe01ebf7fc891bd9290da2
anotherparsa <anotherparsa@gmail.com>
2024-08-28 16:08:32 +0330

    a little change

--- simpleTODO/pkg/admin/adminhome/adminhome.go
+++ simpleTODO/pkg/admin/adminhome/adminhome.go
@@ -4,19 +4,26 @@ import (
 	"html/template"
 	"net/http"
 	"todoproject/pkg/admin/adminstatistics"
+	"todoproject/pkg/models"
 	"todoproject/pkg/session"
 )
+
+type datatosend struct {
+	Statistics models.Static
+	Username   string
+}
 
 func AdminHomePageHandler(w http.ResponseWriter, r *http.Request) {
 	cookie, err := r.Cookie("session_id")
 	//check if session_id exists or not, that menas if the user is logged in or not
 	if err == nil && cookie != nil {
-		_, _, _, rule, _ := session.WhoIsThis(cookie.Value)
+		username, _, _, rule, _ := session.WhoIsThis(cookie.Value)
 		//checking if the logged user is admin
 		if rule == "admin" {
 			data := adminstatistics.InitializeStaticsProcess()
+			datatosend := datatosend{Statistics: data, Username: username}
 			template, _ := template.ParseFiles("../../pkg/admin/adminhome/template/adminhome.html")
-			template.Execute(w, data)
+			template.Execute(w, datatosend)
 		} else {
 			http.Redirect(w, r, "/users/home", http.StatusSeeOther)
 		}
